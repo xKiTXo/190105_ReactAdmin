1a7b541259454a4bcf2102c334af824790d43080
xKiTXo <cat.ion2@hotmail.com>
2020-03-21 16:55:09 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React ,{Component}from 'react'
+import {Button,message} from 'antd'
 
 export default class App extends Component{
-
+    handleClick=()=>{
+        message.success('This is a message')
+    }
 
     render(){
         return(
-            <div>Appqwewqewq</div>
+        <>
+            <div>Appsdsd</div>
+            <Button type="primary" onClick={this.handleClick}>ADD</Button>
+        </>
         )
     }
 }
