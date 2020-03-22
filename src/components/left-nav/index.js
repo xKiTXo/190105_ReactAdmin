a9a61dfd067776cc1f38b93259d84b3c4ede0956
xKiTXo <cat.ion2@hotmail.com>
2020-03-22 22:33:03 +0800

    commit

--- src/components/left-nav/index.js
+++ src/components/left-nav/index.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+
+import { Menu } from 'antd';
+import {
+    HomeOutlined,
+    MailOutlined,
+} from '@ant-design/icons';
 
 import logo from '../../assets/images/logo.png'
 import './index.less'
+
+const { SubMenu } = Menu;
 
 export default class LeftNav extends Component{
 
     render(){
         return(
-            <div className='left-nav'>
-                <header className="left-nav-header">
-                    <img src={logo} alt='logo'/>
-                    <h1>硅谷後台</h1>
-                </header>
+            <div>
+                <div className='left-nav'>
+                    <Link to='/home' className="left-nav-header">
+                        <img src={logo} alt='logo'/>
+                        <h1>硅谷後台</h1>
+                    </Link>
+                </div>
+
+                <Menu
+                    mode="inline"
+                    theme="dark"
+                >
+                    <Menu.Item key="1">
+                        <HomeOutlined  />
+                        <span>首頁</span>
+                    </Menu.Item>
+
+                    <SubMenu
+                        key="sub1"
+                        title=
+                        {
+                            <span>
+                                <MailOutlined />
+                                <span>商品</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="5">
+                            <MailOutlined />
+                                品類管理
+                        </Menu.Item>
+                        <Menu.Item key="6">
+                            <MailOutlined />
+                                商品管理
+                        </Menu.Item>
+                    </SubMenu>
+
+
+                </Menu>
             </div>
         )
     }
--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import {Route,Switch ,Redirect } from 'react-router-dom';
 import { Layout } from 'antd';
 
-import memoryUtils from '../../utils/memoryUtils'
 
+import memoryUtils from '../../utils/memoryUtils'
 import LeftNav from '../../components/left-nav'
 import Header from '../../components/header'
+import Home from '../home/home'
+import Category from '../category/category'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
 
 const { Footer, Sider, Content } = Layout;
 
@@ -19,16 +27,34 @@ class Admin extends Component{
         }
 
         return(
+            
             <Layout style={{height:'100%'}}>
                 <Sider>
                     <LeftNav/>
                 </Sider>
                 <Layout>
-                    <Header/>
-                    <Content style={{backgroundColor:'#fff'}}>Content</Content>
+                    {/* <Header/> */}
+                    <Header>Header</Header>
+                    <Content  style={{backgroundColor:'#fff'}}>
+                   
+                          <Switch>
+                            <Route path='/home' component={Home}/>
+                            <Route path='/category' component={Category}/>
+                            <Route path='/product' component={Product}/>
+                            <Route path='/role' component={Role}/>
+                            <Route path='/user' component={User}/>
+                            <Route path='/charts/bar' component={Bar}/>
+                            <Route path='/charts/line' component={Line}/>
+                            <Route path='/charts/pie' component={Pie}/>
+                            <Redirect to='/home/'/>
+                        </Switch>    
+                        
+                        
+                    </Content >
                     <Footer style={{textAlign:'center',color:'#cccccc'}}>推薦使用谷歌瀏覽器, 可以獲得更佳頁面操作體驗</Footer>
                 </Layout>
             </Layout>
+            
         )
     }
 }
