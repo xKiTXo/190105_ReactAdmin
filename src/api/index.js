3615a49692665774554e871371989dd5c8c35305
xKiTXo <cat.ion2@hotmail.com>
2020-03-23 17:30:18 +0800

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import ajax from './ajax'
-
+import jsonp from 'jsonp'
 // const BASE ='http://localhost:5000';
 const BASE ='';
 //Login
@@ -11,8 +11,8 @@ export const reqAddUser=(user)=>ajax(BASE+'/manage/user/add',user,'POST')
 //jsonp 
 export const reqWeather =(city)=>{
     const url =`http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=E4805d16520de693a3fe707cdc962045`
-    // jsonp(url,{},(err,data)=>{
-    //     console.log('jsonp()',err,data);
-    // })
+    jsonp(url,{},(err,data)=>{
+        console.log('jsonp()',err,data);
+    })
 }
-// reqWeather('北京')
+ reqWeather('北京')
