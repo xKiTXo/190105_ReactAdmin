96919b4900bc1e6d1ddaeec463739b52f1cb1db4
xKiTXo <cat.ion2@hotmail.com>
2020-03-25 16:55:59 +0800

    commit

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch, Redirect} from 'react-router-dom'
 
 import ProductHome from './home'
 import ProductAddUpdate from './add-update'
@@ -11,9 +11,10 @@ export default class Product extends Component{
     render(){
         return(
             <Switch>
-                <Route path='/product/' exact component={ProductHome}/>
+                <Route path='/product' exact component={ProductHome}/>
                 <Route path='/product/addupdate/' component={ProductAddUpdate}/>
                 <Route path='/product/detail/' component={ProductDetail}/>
+                <Redirect to='/product'/>
             </Switch>
         )
     }
